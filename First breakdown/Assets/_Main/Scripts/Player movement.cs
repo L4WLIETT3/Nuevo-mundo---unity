6e6420079c2d9dc445747d52ec83d6256a6b9522
L4WLIETT3 <1sa.alvarezz.g@gmail.com>
2026-07-30 11:51:49 -0500

    player movement

---
+++ First breakdown/Assets/_Main/Scripts/Player movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public float fuerza = 500f;
+
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D.AddForce(Vector2.right * fuerza);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
