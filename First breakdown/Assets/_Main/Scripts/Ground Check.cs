4dfdacf33a4c4f88f11d2f64644325bbb823134b
L4WLIETT3 <1sa.alvarezz.g@gmail.com>
2026-08-11 11:57:43 -0500

    Ground check

---
+++ First breakdown/Assets/_Main/Scripts/Ground Check.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool isGround; 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGround = true;
+        Debug.Log("Me tocaron !!");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGround = false;
+        Debug.Log("Me dejaron de tocar!!");
+    }
+}
