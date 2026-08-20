c779c684ba787f5a4e80d88a773c88a6aa7b9345
L4WLIETT3 <1sa.alvarezz.g@gmail.com>
2026-08-20 11:52:09 -0500

    Trabajo en clase (diseño de nivel)

--- First breakdown/Assets/_Main/Scripts/Ground Check.cs
+++ First breakdown/Assets/_Main/Scripts/Ground Check.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public bool isGround; 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         isGround = true;
         Debug.Log("Me tocaron !!");
